6155a68bb196a72b8609d9250a764075673ee17e
Chitranshi <chitranshi@zib.de>
2024-11-03 14:14:42 +0100

    add data seeds

--- vlm_eval/create_clip_dataset.py
+++ vlm_eval/create_clip_dataset.py
@@ -1,5 +1,6 @@
 from torch.utils.data import Dataset
 from torch.utils.data import Subset
+import torch
 from coco_cf_loader import COCO_CF_dataset, MS_COCO_dataset
 
 
@@ -8,9 +9,10 @@ from coco_cf_loader import COCO_CF_dataset, MS_COCO_dataset
 
 def main():
 
+    torch.manual_seed(42)
     # Intialising seeds for data
     data_seeds = [i for i in range(107,122)]
 
     ms_coco_base_dir = "/home/htc/kchitranshi/SCRATCH/MS_COCO"
     apgd_attacks_base_dir = "/home/htc/kchitranshi/SCRATCH/APGD_SAMPLES"
-
+    
