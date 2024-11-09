2fa260409545e3e005bfd1d9f0e623c09b8001c7
Chitranshi <chitranshi@zib.de>
2024-11-09 09:28:17 +0100

    add complete code

--- vlm_eval/clip_classification.py
+++ vlm_eval/clip_classification.py
@@ -4,6 +4,7 @@ import argparse
 import torch
 from torch.utils.data import DataLoader
 from tqdm import tqdm
+from datasets_classes_templates import data_seeds
 
 def zeroshot_classifier(classnames, templates, processor, model):
     with torch.no_grad():
@@ -35,6 +36,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--data", type=str, default=None, choices=['MS_COCO','medium','base','all'], help='Data on which clip was fine-tuned')
     parser.add_argument("--dataset", type=str, default="CIFAR10", choices=["CIFAR10", "CIFAR100", "ImageNet", "Caltech101", "Caltech256", "Food101"])
+    parser.add_argument("--method",type=str, default="COCO_CF", choices=['COCO_CF','APGD_1','APGD_4','NONE'])
     args = parser.parse_args()
     
     if args.dataset == "CIFAR10":
@@ -65,42 +67,48 @@ def main():
     print(f'Conducting zero-shot image classification on {args.dataset}')
 
     device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+    model_base_path = '/home/htc/kchitranshi/SCRATCH/trained_clip_models'
 
     processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
-    model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32").to(device)
 
-    data_loader = DataLoader(data, batch_size=128, collate_fn=classification_collate_fn)
+    for data_seed in data_seeds:   
+        print(f'Conducting zero-shot image classification on {args.data} with seed {data_seed} for the method {args.method}')
+        model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32").to(device)
+        model.load_state_dict(torch.load(f'{model_base_path}/{args.method}/clip_model_dataset_{args.data}_method_{args.method}_num_epochs_10_data_seed_{data_seed}.pt'))
+        model.eval()
 
-    zeroshot_weights = zeroshot_classifier(classes_templates['classes'], 
-                                           classes_templates['templates'], 
-                                           processor, 
-                                           model
-    )
+        data_loader = DataLoader(data, batch_size=128, collate_fn=classification_collate_fn)
 
-    with torch.no_grad():
-        top1, top5, n = 0., 0., 0.
-        for i, (images, target) in enumerate(tqdm(data_loader)):
-            target = target.to(device)
-            images = list(images)
+        zeroshot_weights = zeroshot_classifier(classes_templates['classes'], 
+                                            classes_templates['templates'], 
+                                            processor, 
+                                            model
+        )
 
-            images = processor(images=images, return_tensors="pt").to(device)
+        with torch.no_grad():
+            top1, top5, n = 0., 0., 0.
+            for i, (images, target) in enumerate(tqdm(data_loader)):
+                target = target.to(device)
+                images = list(images)
 
-            # predict
-            image_features = model.get_image_features(images['pixel_values']).to(device)
-            image_features /= image_features.norm(dim=-1, keepdim=True)
-            logits = 100. * image_features @ zeroshot_weights
+                images = processor(images=images, return_tensors="pt").to(device)
 
-            # measure accuracy
-            acc1, acc5 = accuracy(logits, target, topk=(1, 5))
-            top1 += acc1
-            top5 += acc5
-            n += image_features.size(0)
+                # predict
+                image_features = model.get_image_features(images['pixel_values']).to(device)
+                image_features /= image_features.norm(dim=-1, keepdim=True)
+                logits = 100. * image_features @ zeroshot_weights
 
-    top1 = (top1 / n) * 100
-    top5 = (top5 / n) * 100 
+                # measure accuracy
+                acc1, acc5 = accuracy(logits, target, topk=(1, 5))
+                top1 += acc1
+                top5 += acc5
+                n += image_features.size(0)
 
-    print(f"Top-1 accuracy: {top1:.2f}")
-    print(f"Top-5 accuracy: {top5:.2f}")
+        top1 = (top1 / n) * 100
+        top5 = (top5 / n) * 100 
+
+        print(f"Top-1 accuracy: {top1:.2f}")
+        print(f"Top-5 accuracy: {top5:.2f}")
 
 
 
