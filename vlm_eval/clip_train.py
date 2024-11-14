cf8c1c96d6a5f9356d94ace89ee8fb799b0d875e
Chitranshi <chitranshi@zib.de>
2024-11-14 19:04:13 +0100

    add timm lr schedulr and running loss

--- vlm_eval/clip_train.py
+++ vlm_eval/clip_train.py
@@ -3,7 +3,7 @@ from typing import Callable
 import torch
 from tqdm import tqdm
 from transformers import CLIPProcessor, CLIPModel
-
+from timm.scheduler import CosineLRScheduler
 
 
 
@@ -16,12 +16,13 @@ class ModelTrainer:
                  train_data_loader: torch.utils.data.DataLoader, 
                  val_data_loader: torch.utils.data.DataLoader,
                  num_epochs: int, 
-                 learning_rate: float = 5e-7, 
+                 learning_rate: float = 5e-5, 
                  weight_decay: float = 1e-3,
                  device: str = "cuda:0",
                  save_model: bool = False,
                  save_model_path: str = "/home/htc/kchitranshi/SCRATCH/",
-                 data_seed: int = 42
+                 data_seed: int = 42,
+                 method="COCO_CF",
     ) -> None:
 
         self.model = model
@@ -36,6 +37,7 @@ class ModelTrainer:
         self.save_model = save_model
         self.save_model_path = save_model_path
         self.data_seed = data_seed
+        self.method = method
 
         self.optimizer = torch.optim.AdamW(
             self.model.parameters(),
@@ -46,15 +48,26 @@ class ModelTrainer:
     
     def train(self):
         self.model.train()
+        lr_scheduler = CosineLRScheduler(
+            self.optimizer,
+            t_initial=self.num_epochs * len(self.train_data_loader),
+            lr_min=5e-7,
+            warmup_lr_init=1e-6,
+            warmup_prefix=True,
+            warmup_t=3,
+            cycle_limit=1,
+            t_in_epochs=False,
+        )
         progress_bar = tqdm(range(self.num_epochs))
         for epoch in progress_bar:
+            running_loss = 0.0
             for batch_idx, batch in enumerate(self.train_data_loader):
                 self.optimizer.zero_grad()
                 processed_input = self.processor(text=batch["caption"], 
                                                  images=batch["image"], 
                                                  return_tensors="pt", 
                                                  padding=True, 
-                                                 max_length=77, 
+                                                 max_length=128, 
                                                  truncation=True
                 )
                 outputs = self.model(input_ids=processed_input['input_ids'].squeeze().to(self.device),
@@ -64,18 +77,20 @@ class ModelTrainer:
                 )
                 loss = outputs.loss
                 loss.backward()
+                running_loss += loss.item() * len(batch["caption"])
                 self.optimizer.step()
+                lr_scheduler.step_update(batch_idx + epoch * len(self.train_data_loader))
 
-            print(f"Epoch {epoch+1}/{self.num_epochs} Loss: {loss.item()}")
+            print(f"Epoch {epoch+1}/{self.num_epochs} Loss: {running_loss/len(self.train_data_loader.dataset):.4f}")
             progress_bar.set_postfix(
                 epoch="{}/{}".format(epoch+1,self.num_epochs),
                 loss=loss.item(),
-                lr=self.learning_rate
+                lr=self.optimizer.param_groups[0]["lr"]
             )
 
         if self.save_model:
-            torch.save(self.model.state_dict(), self.save_model_path + f'clip_model_dataset_{self.data_name}_num_epochs_{self.num_epochs}_data_{self.data_name}_data_seed_{self.data_seed}.pt')
-            print(f"Saving fine-tuned model as clip_model_dataset_{self.data_name}_num_epochs_{self.num_epochs}_data_{self.data_name}_data_seed_{self.data_seed}.pt")
+            torch.save(self.model.state_dict(), self.save_model_path + f'clip_model_dataset_{self.data_name}_method_{self.method}_num_epochs_{self.num_epochs}_data_seed_{self.data_seed}.pt')
+            print(f"Saving fine-tuned model as clip_model_dataset_{self.data_name}_method_{self.method}_num_epochs_{self.num_epochs}_data_seed_{self.data_seed}.pt")
     
     def eval(self):
         self.model.eval()
@@ -86,14 +101,14 @@ class ModelTrainer:
             processed_input = self.processor(text=batch["caption"], 
                                                  images=batch["image"], 
                                                  return_tensors="pt", 
-                                                 padding=True, 
-                                                 max_length=77, 
+                                                 padding='max_length', 
+                                                 max_length=128, 
                                                  truncation=True
                 )
             outputs = self.model(
-                input_ids=processed_input['input_ids'].squeeze(),
-                attention_mask=processed_input['attention_mask'].squeeze(),
-                pixel_values=processed_input['pixel_values'].squeeze(),
+                input_ids=processed_input['input_ids'].squeeze().to(self.device),
+                attention_mask=processed_input['attention_mask'].squeeze().to(self.device),
+                pixel_values=processed_input['pixel_values'].squeeze().to(self.device),
                 return_loss=True)
             loss, logits_per_image = outputs.loss, outputs.logits_per_image 
             epoch_loss += loss.item()
@@ -113,7 +128,10 @@ def main():
     parser.add_argument('--num_epochs', type=int, default=20)
     parser.add_argument('--data_seed', type=int, default=42)
     parser.add_argument('--data_name', type=str, default="MS_COCO", choices=["MS_COCO","base","medium","all"])
+    parser.add_argument('--learning_rate', type=float, default=1e-5)
+    parser.add_argument('--batch_size', type=int, default=32)
     parser.add_argument('--save_model', action='store_true', default=False)
+    parser.add_argument('--method', type=str, choices=['COCO_CF','APGD_1','APGD_4','NONE'])
     parser.add_argument('--save_model_path', type=str, default="/home/htc/kchitranshi/SCRATCH/")
     parser.add_argument(
         "--data_seeds",
@@ -135,8 +153,19 @@ def main():
 
 
     for data_seed in args.data_seeds:
-        print(f"Data Seed: {data_seed} Data Name: {args.data_name}")
-        dataset = MS_COCO_dataset(base_dir='/home/htc/kchitranshi/SCRATCH/Datasets/MS_COCO_APGD_4', annotation_file=f'/json_files/data_name_{args.data_name}_data_seed_{data_seed}.json')
+        
+        if args.data_name not in ['MS_COCO', 'all']:
+            print(f"Data Seed: {data_seed} | Data Name: {args.data_name} | Method: {args.method}")
+            dataset = MS_COCO_dataset(base_dir=f'/home/htc/kchitranshi/SCRATCH/Datasets/MS_COCO_{args.method}',
+                                      annotation_file=f'/json_files/data_name_{args.data_name}_data_seed_{data_seed}.json')
+        elif args.data_name == 'all':
+            print(f"Data Name: {args.data_name} | Method: {args.method}")
+            dataset = MS_COCO_dataset(base_dir=f'/home/htc/kchitranshi/SCRATCH/Datasets/MS_COCO_{args.method}',
+                                      annotation_file=f'/json_files/data_name_{args.data_name}.json')
+        else:
+            print(f"Data Name: {args.data_name} | Method: {args.method}")
+            dataset = MS_COCO_dataset(base_dir=f'/home/htc/kchitranshi/SCRATCH/Datasets/MS_COCO',
+                                      annotation_file=f'/json_files/ms_coco_captions_data_{data_seed}.json')
 
         train_size = int(0.8 * len(dataset))  # 80% for training
         val_size = len(dataset) - train_size   # 20% for validation
@@ -145,7 +174,7 @@ def main():
         train_dataset, val_dataset = torch.utils.data.random_split(dataset, [train_size, val_size])
 
         # Optional: Create DataLoaders for each subset
-        train_loader = DataLoader(train_dataset, batch_size=128, shuffle=True, collate_fn=custom_collate_fn)
+        train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, collate_fn=custom_collate_fn)
         val_loader = DataLoader(val_dataset, batch_size=128, shuffle=False, collate_fn=custom_collate_fn)
 
         trainer = ModelTrainer(model=model, 
@@ -154,14 +183,19 @@ def main():
                             train_data_loader=train_loader,
                             val_data_loader=val_loader, 
                             num_epochs=args.num_epochs, 
-                            learning_rate=5e-7, 
+                            learning_rate=args.learning_rate, 
                             weight_decay=1e-3,
                             device=device,
-                            data_seed=args.data_seed,
-                            save_model=args.save_model
+                            data_seed=data_seed,
+                            save_model=args.save_model,
+                            save_model_path=args.save_model_path,
+                            method=args.method,
         )
 
         trainer.train()
+
+        if args.data_name in ['MS_COCO','all']:
+            break
 
 
 if __name__ == "__main__":
